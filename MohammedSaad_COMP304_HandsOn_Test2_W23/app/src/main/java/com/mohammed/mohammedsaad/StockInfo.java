5c669501aa2e51261a59767238074c48dfa41c08
Mohammed Saad <msaad@mail.bradley.edu>
2024-08-16 16:47:42 -0400

    test2 update2

--- MohammedSaad_COMP304_HandsOn_Test2_W23/app/src/main/java/com/mohammed/mohammedsaad/StockInfo.java
+++ MohammedSaad_COMP304_HandsOn_Test2_W23/app/src/main/java/com/mohammed/mohammedsaad/StockInfo.java
@@ -2,21 +2,23 @@ package com.mohammed.mohammedsaad;
 
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
+import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
-
 
 @Entity(tableName = "stock_info")
 public class StockInfo {
     @PrimaryKey
     @NonNull
-    private String stockSymbol;
+    private String stockSymbol = ""; // Initialize with a default value
 
     private String companyName;
     private double stockQuote;
 
-    // Constructors
+    // Default constructor (needed by Room)
     public StockInfo() {}
 
+    // Constructor with parameters
+    @Ignore // Tell Room to ignore this constructor
     public StockInfo(@NonNull String stockSymbol, String companyName, double stockQuote) {
         this.stockSymbol = stockSymbol;
         this.companyName = companyName;
